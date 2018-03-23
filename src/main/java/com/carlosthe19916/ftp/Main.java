161599d67fb9b2c3ee7000d2bb8fc2407fc62fcc
Carlos Feria <carlosthe19916@gmail.com>
2018-03-22 19:15:25 -0500

    Files

---
+++ src/main/java/com/carlosthe19916/ftp/Main.java
@@ -0,0 +1,31 @@
+package com.carlosthe19916.ftp;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+
+        CamelContext context = new DefaultCamelContext();
+        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox?noop=true").to("jms:queue:incomingOrders");
+            }
+        });
+
+        context.start();
+        Thread.sleep(10_000);
+
+        context.stop();
+    }
+}
